418c4389527e5cc958596fafa29bd36a9a75fc4b
zhishengliu <40776894+zhishengliu1@users.noreply.github.com>
2025-06-25 16:14:51 -0700

    Add asset path to fix Vercel preview (#1)
    * Add asset path
    
    * Fix CI
    
    * fix Vercel preview

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "memo-website";
+
+// This ensures Vercel and local development use root paths, while GitHub Pages uses the correct prefix.
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   /* config options here */
 };
 
