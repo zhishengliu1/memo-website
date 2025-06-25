7b28563d8d79dde3983920e2de4ff137290e9297
Zhi <zhishengliu1@gmail.com>
2025-06-25 16:01:10 -0700

    Add asset path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const repo = "memo-website";
+
 const nextConfig: NextConfig = {
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   /* config options here */
 };
 
