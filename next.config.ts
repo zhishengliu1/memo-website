4f008793d2acd5d216e8e81906000570cd943bb4
Zhi <zhishengliu1@gmail.com>
2025-06-25 16:47:05 -0700

    remove redundant path

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
-const repo = "memo-website";
-
-// This ensures Vercel and local development use root paths, while GitHub Pages uses the correct prefix.
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
-  basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
-  // Expose basePath to the client
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isGithubPages ? `/${repo}` : "",
-  },
-  // ...other config options
 };
 
 export default nextConfig;
