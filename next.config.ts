f6d7c31ab1b325498f8e5e650941252e13cf769c
Zhi <zhishengliu1@gmail.com>
2025-06-25 16:12:09 -0700

    fix Vercel preview

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const repo = "memo-website";
 
+// This ensures Vercel and local development use root paths, while GitHub Pages uses the correct prefix.
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   /* config options here */
 };
 
