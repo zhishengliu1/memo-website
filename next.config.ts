f2714d199721a0197f06f10c4385b5514aafa20f
Zhi <zhishengliu1@gmail.com>
2025-06-25 18:04:28 -0700

    remove config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = "memo-website";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
+  // basePath: isGithubPages ? `/${repo}` : "",
+  // assetPrefix: isGithubPages ? `/${repo}/` : "",
+  /* config options here */
 };
 
 export default nextConfig;
