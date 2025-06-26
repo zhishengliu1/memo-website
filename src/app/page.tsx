a565a3f57f68b29f80e7734956d6fd439c903588
zhishengliu <40776894+zhishengliu1@users.noreply.github.com>
2025-06-25 20:20:17 -0700

    Remove redundant export configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your change instantly.
           </li>
         </ol>
 
